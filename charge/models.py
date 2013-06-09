63c1f42644ddf2dbf2acce86d886f05e2c7541a1
liuzhijun <liuzhijun@gmail.com>
2013-06-09 16:13:46 +0800

    init

---
+++ charge/models.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python 
+#encoding:utf-8
+from django.db import models
+from datetime import datetime
+from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+
+# Create your models here.
+
+class Charge(models.Model):
+    amount = models.DecimalField(max_digits=3,decimal_places=1,\
+    verbose_name=u"消费金额",default=0)
+    usage = models.CharField(max_length=50,default=u"吃饭",\
+    verbose_name=u"用途")
+    add_date = models.DateTimeField(default=datetime.now(),verbose_name=u"添加日期")
+
+
+class ChargeAdmin(admin.ModelAdmin):
+    list_display = ('amount','usage','add_date')
+    search_fields = ('usage',)
+    list_filter  = (('add_date',DateFieldListFilter),)
+    #change_list_template = 'extras/change_form.html'
+
+
+
+admin.site.register(Charge,ChargeAdmin)
+
+
+
+
+
+
+    
